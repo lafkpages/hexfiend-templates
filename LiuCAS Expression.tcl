dad951e6dee5130e0f6318f1f65510e83ca55b55
lafkpages <luisafk@proton.me>
2025-10-24 10:07:32 +0200

    feat(liucas): improve and update

--- LiuCAS Expression.tcl
+++ LiuCAS Expression.tcl
@@ -7,28 +7,29 @@ include "hexfiend-templates/helpers/util.tcl"
 set expression_kinds(0) "Addition"
 set expression_kinds(1) "BooleanAnd"
 set expression_kinds(2) "BooleanFalse"
-set expression_kinds(3) "BooleanOr"
-set expression_kinds(4) "BooleanTrue"
-set expression_kinds(5) "Ceil"
-set expression_kinds(6) "Cos"
-set expression_kinds(7) "EulerNumber"
-set expression_kinds(8) "Floor"
-set expression_kinds(9) "Fraction"
-set expression_kinds(10) "GCD"
-set expression_kinds(11) "LCM"
-set expression_kinds(12) "Log"
-set expression_kinds(13) "Mod"
-set expression_kinds(14) "Number"
-set expression_kinds(15) "Pi"
-set expression_kinds(16) "Placeholder"
-set expression_kinds(17) "Power"
-set expression_kinds(18) "Product"
-set expression_kinds(19) "ProductSequence"
-set expression_kinds(20) "Scoped"
-set expression_kinds(21) "Sin"
-set expression_kinds(22) "SquareRoot"
-set expression_kinds(23) "SumSequence"
-set expression_kinds(24) "Unknown"
+set expression_kinds(3) "BooleanNot"
+set expression_kinds(4) "BooleanOr"
+set expression_kinds(5) "BooleanTrue"
+set expression_kinds(6) "Ceil"
+set expression_kinds(7) "Cos"
+set expression_kinds(8) "EulerNumber"
+set expression_kinds(9) "Floor"
+set expression_kinds(10) "Fraction"
+set expression_kinds(11) "GCD"
+set expression_kinds(12) "LCM"
+set expression_kinds(13) "Log"
+set expression_kinds(14) "Mod"
+set expression_kinds(15) "Number"
+set expression_kinds(16) "Pi"
+set expression_kinds(17) "Placeholder"
+set expression_kinds(18) "Power"
+set expression_kinds(19) "Product"
+set expression_kinds(20) "ProductSequence"
+set expression_kinds(21) "Scoped"
+set expression_kinds(22) "Sin"
+set expression_kinds(23) "SquareRoot"
+set expression_kinds(24) "SumSequence"
+set expression_kinds(25) "Unknown"
 set expression_kinds(255) "_Reserved"
 
 proc read_expression {{key ""}} {
@@ -43,7 +44,9 @@ proc read_expression {{key ""}} {
         }
 
         switch $kind {
-            0 {
+            0 -
+            1 -
+            4 {
                 set terms_count [uint8 "Terms Count"]
 
                 for {set i 0} {$i < $terms_count} {incr i} {
@@ -51,12 +54,12 @@ proc read_expression {{key ""}} {
                 }
             }
 
-            9 {
+            10 {
                 read_expression "Numerator"
                 read_expression "Denominator"
             }
 
-            14 {
+            15 {
                 set byte_length [uint32 "Byte length"]
 
                 set value ""
@@ -89,34 +92,35 @@ proc read_expression {{key ""}} {
                 sectionvalue $value
             }
 
-            17 {
+            18 {
                 read_expression "Base"
                 read_expression "Exponent"
             }
 
-            24 {
+            25 {
                 set name_length [uint8 "Name Length"]
                 set name [str $name_length utf8 "Name"]
                 sectionvalue $name
             }
 
-            6 -
-            21 {
+            7 -
+            22 {
                 # Single argument trigonometric functions
                 read_expression "Angle"
             }
 
-            5 -
-            8 {
+            3 -
+            6 -
+            9 {
                 # Single argument functions
                 read_expression "Value"
             }
 
             2 -
-            4 -
-            7 -
-            15 -
-            16 {
+            5 -
+            8 -
+            16 -
+            17 {
                 # Constants, no additional data
             }
 
